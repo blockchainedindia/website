225bd214ac4fb88562fc65a3bb5aa0c973002de5
Kumar Anirudha <anirudhastark@yahoo.com>
2017-08-08 02:32:39 +0530

    cleaned up

--- client/js/header.js
+++ client/js/header.js
@@ -9,43 +9,9 @@ function mainBannerScroll(scroll) {
     });
 }
 
-//function itemsParallax(scroll) {
-//    var $photoDiv = $('.item-pics');
-//    var threshold = $photoDiv.offset().top - $(window).height();
-//    if (scroll > threshold ) {
-//        $('.item-pics figure').each(function(i) {
-//            setTimeout(function() {
-//                $('.item-pics figure').eq(i).addClass('is-showing');
-//            }, 50 * (i + 1))
-//        })
-//    }
-//}
-
-//function windowParallax(scroll) {
-//    var $target = $(".par-window");
-//    var offset = $target.offset().top;
-//    var threshold =  offset - $(window).height()
-//    var opacity = (scroll - offset + 500) / (scroll/4)
-//
-//    if (scroll > threshold) {
-//        console.log("ya")
-//        $target.css({
-//            'background-position':'center '+ (scroll/5 - offset) +'px'
-//        })
-//        $('.tint').css({
-//            'opacity': opacity
-//        })
-//    }
-//}
-
-
 $(document).ready(function() {
     $(window).scroll(function() {
         var wScroll = $(this).scrollTop();
-        // console.log(wScroll);
         mainBannerScroll(wScroll);
-        //itemsParallax(wScroll);
-        //windowParallax(wScroll);
     });
 });
-
